d73f8931235435ab0413b8a4d1f4a5b04060cbda
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-03-21 21:38:56 -0500

    Debugged all errors& Completed Assignment

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,30 +24,39 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+/*console.log("Welcome to the bootcamp*/
 
 // What’s Wrong?
+
+console.log("Welcome to the bootcamp");
+
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+/*let numbers = [2, 4, "eight"];
+for (let i = 0; i < numbers.length; i++) {
+  let doubled = numbers[i] * 2;
+  console.log(doubled);
+}*/
+
+// What’s Wrong?
+
+
+let numbers = [2, 4, 8]; // Fixed the string issue
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
-
-// What’s Wrong?
-
 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
-function isPrime(num) {
+/*function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
@@ -58,5 +67,19 @@ function isPrime(num) {
 }
 
 console.log(isPrime(7)); // Expected true but gets false
+*/
 
 // What’s Wrong?
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false; // Correctly marks non-prime numbers
+    }
+  }
+  return true; // Correctly marks prime numbers
+}
+
+console.log(isPrime(7)); // Now correctly returns true
+
